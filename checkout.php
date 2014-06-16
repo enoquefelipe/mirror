72923055e5309c132a60c694246f98a6dd2bfbc6
Administrador <Administrador@INFO01.hmsj.net>
2014-06-16 18:41:29 -0300

    Fim do add bootstrap

--- checkout.php
+++ checkout.php
@@ -5,12 +5,13 @@
         <meta charset="UTF-8">
         <title> Checkout Mirror Fashion </title>
         <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
         <meta name="viewport" content="width=device-width">
     </head>
     <body>
 
         <div class="container">
-            
+
             <div class="jumbotron">
                 <div class="container">
                     <!-- h1 e p que já tínhamos -->
@@ -43,6 +44,59 @@
                 </div>
 
             </div>
+
+            <form>
+                <fieldset>
+                    <legend>Dados pessoais</legend>
+                    <div class="form-group">
+                        <label for="nome">Nome completo</label>
+                        <input type="text" class="form-control" id="nome" name="nome" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="email@exemplo.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="cpf">CPF</label>
+                        <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00">
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="sim" name="spam" checked>
+                            Quero receber spam da Mirror Fashion
+                        </label>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Cartão de crédito</legend>
+                    <div class="form-group">
+                        <label for="numero-cartao">Número - CVV</label>
+                        <input type="text" class="form-control"
+                               id="numero-cartao" name="numero-cartao">
+                    </div>
+                    <div class="form-group">
+                        <label for="bandeira-cartao">Bandeira</label>
+                        <select name="bandeira-cartao" id="bandeira-cartao"
+                                class="form-control">
+                            <option value="master">MasterCard</option>
+                            <option value="visa">VISA</option>
+                            <option value="amex">American Express</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="validade-cartao">Validade</label>
+                        <input type="month" class="form-control"
+                               id="validade-cartao" name="validade-cartao">
+                    </div>
+                </fieldset>
+
+                <button type="submit" class="btn btn-primary btn-lg pull-right">
+                    <span class="glyphicon glyphicon-thumbs-up"></span>
+                    Confirmar Pedido
+                </button>
+            </form>
+
         </div>
 
 
